d9a2494c9c12fbf905410bd6d8b0bde4ffdc386d
Moein <tjex31@yandex.com>
2022-05-09 16:52:00 +0430

    feat: User material icons

--- src/views/HomeView.svelte
+++ src/views/HomeView.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
   import { active, Route } from 'tinro'
   import Container from 'typedi'
+  import IconAccountCircle from '~icons/mdi/account-circle'
+  import IconContact from '~icons/mdi/file-document-edit-outline'
+  import IconSetting from '~icons/mdi/tune-vertical'
+  import IconDashboard from '~icons/mdi/view-dashboard'
   import ThemeService from '../services/ThemeService'
 
   const themeService = Container.get(ThemeService)
@@ -20,58 +24,26 @@
       >
         <li class="ml-2 tab__item">
           <a class="group" href="/home/tab1" use:active>
-            <svg
-              class="ml-2 w-5 h-5"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-              ><path
-                fill-rule="evenodd"
-                d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
-                clip-rule="evenodd"
-              /></svg
-            >تایم لاین
+            <IconAccountCircle class="ml-2 w-5 h-5" />
+            تایم لاین
           </a>
         </li>
         <li class="ml-2 tab__item">
           <a href="/home/tab2" class="group" aria-current="page" use:active>
-            <svg
-              class="ml-2 w-5 h-5"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-              ><path
-                d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"
-              /></svg
-            >داشبورد
+            <IconDashboard class="ml-2 w-5 h-5" />
+            داشبورد
           </a>
         </li>
         <li class="ml-2 tab__item">
           <a href="/home/tab3" class="group" use:active>
-            <svg
-              class="ml-2 w-5 h-5 text-gray-400 group-hover:text-gray-500 dark:text-gray-500 dark:group-hover:text-gray-300"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-              ><path
-                d="M5 4a1 1 0 00-2 0v7.268a2 2 0 000 3.464V16a1 1 0 102 0v-1.268a2 2 0 000-3.464V4zM11 4a1 1 0 10-2 0v1.268a2 2 0 000 3.464V16a1 1 0 102 0V8.732a2 2 0 000-3.464V4zM16 3a1 1 0 011 1v7.268a2 2 0 010 3.464V16a1 1 0 11-2 0v-1.268a2 2 0 010-3.464V4a1 1 0 011-1z"
-              /></svg
-            >تنظیمات
+            <IconSetting class="ml-2 w-5 h-5" />
+            تنظیمات
           </a>
         </li>
         <li class="ml-2 tab__item">
           <a href="/home/tab4" class="group" use:active>
-            <svg
-              class="ml-2 w-5 h-5 text-gray-400 group-hover:text-gray-500 dark:text-gray-500 dark:group-hover:text-gray-300"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-              ><path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z" /><path
-                fill-rule="evenodd"
-                d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z"
-                clip-rule="evenodd"
-              /></svg
-            >ارتباط با ما
+            <IconContact class="ml-2 w-5 h-5" />
+            ارتباط با ما
           </a>
         </li>
       </ul>
